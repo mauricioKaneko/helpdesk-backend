2139dbe55162e3fa5780902ec508c57beeb01f49
mauricioKaneko <mauricio.kaneko@gmail.com>
2022-02-20 17:10:12 -0300

    Implementação de Validation

---
+++ src/main/java/com/kaneko/helpdesk/resources/exceptions/ValidationError.java
@@ -0,0 +1,32 @@
+package com.kaneko.helpdesk.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError{
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+
+	public ValidationError() {
+		super();
+	}
+
+	public ValidationError(Long timeStamp, Integer status, String error, String message, String path) {
+		super(timeStamp, status, error, message, path);
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addErrors(String fieldName, String message) {
+		this.errors.add(new FieldMessage(fieldName,message));
+	}
+	
+	
+	
+	
+	
+
+}

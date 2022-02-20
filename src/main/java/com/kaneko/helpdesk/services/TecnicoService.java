bb6b8955ce7431588b42c75daaf782eed5eed454
mauricioKaneko <mauricio.kaneko@gmail.com>
2022-02-20 16:15:05 -0300

    implementado excessão

--- src/main/java/com/kaneko/helpdesk/services/TecnicoService.java
+++ src/main/java/com/kaneko/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.kaneko.helpdesk.domain.Tecnico;
 import com.kaneko.helpdesk.repositories.TecnicoRepository;
+import com.kaneko.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
@@ -15,6 +16,6 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = tecnicoRepository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado para o Id: " + id));
 	}
 }

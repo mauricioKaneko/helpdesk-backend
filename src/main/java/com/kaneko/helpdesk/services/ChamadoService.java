149b36f60a8fc499c10fa9e09df8874d517c464e
mauricioKaneko <mauricio.kaneko@gmail.com>
2022-02-20 17:55:42 -0300

    Implementação findbyAll para chamado

--- src/main/java/com/kaneko/helpdesk/services/ChamadoService.java
+++ src/main/java/com/kaneko/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.kaneko.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,4 +19,10 @@ public class ChamadoService {
 		Optional<Chamado> obj = chamadoRepository.findById(id);
 		return obj.orElseThrow(()->new ObjectNotFoundException("Chamado não encontrado! ID: " + id));
 	}
+
+	public List<Chamado> findAll() {
+		return chamadoRepository.findAll();
+	}
+	
+	
 }

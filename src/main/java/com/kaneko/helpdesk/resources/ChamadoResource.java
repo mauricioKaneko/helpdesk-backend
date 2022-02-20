d9cb7c8f2daa187047e59d0853678bbb2f52a0c1
mauricioKaneko <mauricio.kaneko@gmail.com>
2022-02-20 18:08:47 -0300

    Implementação create para chamado

--- src/main/java/com/kaneko/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/kaneko/helpdesk/resources/ChamadoResource.java
@@ -1,5 +1,6 @@
 package com.kaneko.helpdesk.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.kaneko.helpdesk.domain.Chamado;
 import com.kaneko.helpdesk.domain.dtos.ChamadoDTO;
@@ -30,6 +34,13 @@ public class ChamadoResource {
 		List<Chamado> list = service.findAll();
 		List<ChamadoDTO> listDTO = list.stream().map( obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ChamadoDTO> create(@RequestBody ChamadoDTO chamadoDTO){
+		Chamado chamado = service.create(chamadoDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(chamado.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
--- src/main/java/com/kaneko/helpdesk/services/ChamadoService.java
+++ src/main/java/com/kaneko/helpdesk/services/ChamadoService.java
@@ -7,6 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kaneko.helpdesk.domain.Chamado;
+import com.kaneko.helpdesk.domain.Cliente;
+import com.kaneko.helpdesk.domain.Tecnico;
+import com.kaneko.helpdesk.domain.dtos.ChamadoDTO;
+import com.kaneko.helpdesk.domain.enums.Prioridade;
+import com.kaneko.helpdesk.domain.enums.Status;
 import com.kaneko.helpdesk.repositories.ChamadoRepository;
 import com.kaneko.helpdesk.services.exceptions.ObjectNotFoundException;
 
@@ -14,6 +19,12 @@ import com.kaneko.helpdesk.services.exceptions.ObjectNotFoundException;
 public class ChamadoService {
 	@Autowired
 	private ChamadoRepository chamadoRepository;
+	
+	@Autowired
+	private TecnicoService tecnicoService;
+	
+	@Autowired
+	private ClienteService clienteService;
 	
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = chamadoRepository.findById(id);
@@ -23,6 +34,28 @@ public class ChamadoService {
 	public List<Chamado> findAll() {
 		return chamadoRepository.findAll();
 	}
+
+	public Chamado create(ChamadoDTO chamadoDTO) {
+		return chamadoRepository.save(newChamado(chamadoDTO));
+	}
 	
-	
+	private Chamado newChamado(ChamadoDTO obj) {
+		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+		Cliente cliente = clienteService.findById(obj.getCliente());
+		
+		Chamado chamado = new Chamado();
+		if(obj.getId() != null) {
+			chamado.setId(obj.getId());
+		}
+		
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEmum(obj.getPrioridade()));
+		chamado.setStatus(Status.toEmum(obj.getStatus()));
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoes(obj.getObservacoes());
+		
+		return chamado;
+		
+	}
 }

69b5227996d294dde2500aaec6f7d97bacb28d77
mauricioKaneko <mauricio.kaneko@gmail.com>
2022-02-20 16:23:55 -0300

    criacao endopoint findall para tecnicos

--- src/main/java/com/kaneko/helpdesk/services/DBService.java
+++ src/main/java/com/kaneko/helpdesk/services/DBService.java
@@ -27,15 +27,22 @@ public class DBService {
 	ChamadoRepository chamadoRepository;
 	
 	public void instanciaDB() {
-		Tecnico tec1 = new Tecnico(null, "Mauricio Kaneko", "26906383809","mauricio.kaneko@gmail.com","12345");
-		tec1.addPerfil(Perfil.ADMIN);
+		Tecnico tec1 = new Tecnico(null, "Mauricio", "00524588709","mauricio.kaneko@gmail.com","12345");
+		tec1.addPerfil(Perfil.TECNICO);
+		Tecnico tec2 = new Tecnico(null, "Kendi", "26906383809","kendi.kaneko@gmail.com","12345");
+		tec2.addPerfil(Perfil.TECNICO);
+		Tecnico tec3 = new Tecnico(null, "Kaneko", "021574154806","kaneko.kaneko@gmail.com","12345");
+		tec3.addPerfil(Perfil.TECNICO);
+
 		Cliente cli1 = new Cliente(null, "Elisa Yokomizo", "21688827897", "elisa.yokomizo@gmail.com", "98765");
 		cli1.addPerfil(Perfil.CLIENTE);
 		Chamado chm1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro Chamado",
 				tec1, cli1);
 		
 		tecnicoRepository.saveAll(Arrays.asList(tec1));
-		clienteRepository.saveAll(Arrays.asList(cli1));
+		tecnicoRepository.saveAll(Arrays.asList(tec2));
+		tecnicoRepository.saveAll(Arrays.asList(tec3));
+		clienteRepository.saveAll(Arrays.asList(cli1));  
 		chamadoRepository.saveAll(Arrays.asList(chm1));
 	}
 }
--- src/main/java/com/kaneko/helpdesk/services/TecnicoService.java
+++ src/main/java/com/kaneko/helpdesk/services/TecnicoService.java
@@ -1,5 +1,6 @@
 package com.kaneko.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,5 +18,10 @@ public class TecnicoService {
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = tecnicoRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado para o Id: " + id));
+	}
+
+	public List<Tecnico> findAll() {
+		
+		return tecnicoRepository.findAll();
 	}
 }

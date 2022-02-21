f231a672cb09a785a0a290f7d5b98cbb6176e88c
mauricioKaneko <mauricio.kaneko@gmail.com>
2022-02-21 07:49:58 -0300

    Autorização com JWT

--- src/main/java/com/kaneko/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/kaneko/helpdesk/resources/TecnicoResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -41,7 +42,7 @@ public class TecnicoResource {
 		List<TecnicoDTO> listDTO = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
-	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@PostMapping
 	public ResponseEntity<TecnicoDTO> create(@Valid @RequestBody TecnicoDTO tecnicoDTO){
 		Tecnico newTecnico = tecnicoService.create(tecnicoDTO);
@@ -49,6 +50,8 @@ public class TecnicoResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
+		
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<TecnicoDTO> update(@PathVariable Integer id,@Valid @RequestBody TecnicoDTO tecnicoDTO){
 		Tecnico obj = tecnicoService.update(id, tecnicoDTO);
